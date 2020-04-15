d4045f0a92adf10d5a3c3eb878a8c2f9fd9a05d1
happyPeiLin <18398322316@163.com>
2020-04-15 17:12:08 +0800

    7

---
+++ src/pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <span class="msg">hello world</span>
+    <van-button type="primary" @click="jump">按钮</van-button>
+  </div>
+</template>
+
+<script>
+import { post } from "@/utils/request"
+export default {
+  methods:{
+    jump(){
+      post("https://www.baidu.com").then(response=>{
+        console.log("handleResponse...")
+      })
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.msg {
+  color: red;
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
